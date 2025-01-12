fc28bdf83eb2ba7ae96c2e005bcd7a5d6a8a6bf3
catta1eya <p.kubica1605@gmail.com>
2025-01-12 22:55:39 +0100

    Initial commit

---
+++ src/components/NextButton.jsx
@@ -0,0 +1,26 @@
+const NextButton = ({ dispatch, answer, isLastQuestion }) => {
+  if (answer === null) return;
+
+  if (!isLastQuestion)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "nextQuestion" })}
+      >
+        Next
+      </button>
+    );
+
+  if (isLastQuestion) {
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
+  }
+};
+
+export default NextButton;
